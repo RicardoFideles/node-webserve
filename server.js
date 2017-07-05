41e131324c1d40d0544e4e3eb1fccb5abf927aea
Ricardo Fideles <ricardo@ricardofideles.com.br>
2017-07-05 10:23:04 -0300

    removing maitence

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintence.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintence.hbs');
+// });
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
